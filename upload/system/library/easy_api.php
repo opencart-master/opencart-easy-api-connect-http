ebe0c80b66e0ac7be9831446053f6409c9744f0c
deniscunha <ded_2004@hotmail.com>
2026-06-06 18:36:20 -0300

    init class php

---
+++ upload/system/library/easy_api.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package		OpenCart
+ * @author		Denis Cunha
+ * @copyright	Copyright (c) 2005 - 2017, OpenCart, Ltd. (https://www.opencart.com/)
+ * @license		https://opensource.org/license/mit
+ * @link		https://www.opencartmaster.com.br
+*/
+class EasyApi {
+
+
+}
