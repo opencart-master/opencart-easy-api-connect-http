41f992ace4158b24359c370e6fb467ed1fd64f00
deniscunha <ded_2004@hotmail.com>
2026-06-06 18:41:00 -0300

    init function construct

--- upload/system/library/easy_api.php
+++ upload/system/library/easy_api.php
@@ -2,11 +2,15 @@
 /**
  * @package		OpenCart
  * @author		Denis Cunha
- * @copyright	Copyright (c) 2005 - 2017, OpenCart, Ltd. (https://www.opencart.com/)
+ * @copyright	Copyright (c) 2005 - 2026, OpenCart, Ltd. (https://www.opencart.com/)
  * @license		https://opensource.org/license/mit
  * @link		https://www.opencartmaster.com.br
 */
 class EasyApi {
+    private $url;
 
+    public function __construct($url) {
+		$this->url = $url;
+	}
 
 }
