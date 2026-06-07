d15dc9d1f54e057c801ee9eeb0981379faa3027d
deniscunha <ded_2004@hotmail.com>
2026-06-07 12:21:49 -0300

    insert function post

--- upload/system/library/easy_api.php
+++ upload/system/library/easy_api.php
@@ -14,24 +14,27 @@ class EasyApi {
 	}
 
   private function request($method, $header, $endpoint, $data = []) {
-        $soap_do = curl_init($this->url . $endpoint);
-        curl_setopt($soap_do, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($soap_do, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($soap_do, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($soap_do, CURLOPT_HTTPHEADER, $header);
-        if ($method !== 'GET') {
-          curl_setopt($soap_do, CURLOPT_CUSTOMREQUEST, $method);
-          curl_setopt($soap_do, CURLOPT_POSTFIELDS, $data);
-        }
-        $response = curl_exec($soap_do);
-        curl_close($soap_do);
-        $resposta = json_decode($response, true);
-        return $resposta;
+    $soap_do = curl_init($this->url . $endpoint);
+    curl_setopt($soap_do, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($soap_do, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($soap_do, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($soap_do, CURLOPT_HTTPHEADER, $header);
+    if ($method !== 'GET') {
+      curl_setopt($soap_do, CURLOPT_CUSTOMREQUEST, $method);
+      curl_setopt($soap_do, CURLOPT_POSTFIELDS, $data);
+    }
+    $response = curl_exec($soap_do);
+    curl_close($soap_do);
+    $resposta = json_decode($response, true);
+    return $resposta;
   }
 
   public function get($header, $endpoint) {
+    return $this->request('GET', $header, $endpoint, '');
+  }
 
-  return $this->request('GET', $header, $endpoint, '');
+  public function post($header, $endpoint, $data) {
+    return $this->request('POST', $header, $endpoint, $data);
   }
 
 }
